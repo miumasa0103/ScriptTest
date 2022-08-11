4fa224801ce5239bc7472657246e90be2a5a1bdc
miumasa0103 <108392094+miumasa0103@users.noreply.github.com>
2022-08-12 01:57:39 +0900

    発展課題仕様へ変更
    レッスン４の課題をプッシュした後、発展課題をTest.csで実行したため。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,26 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int hp = 50; //体力
+    private int power = 10; //攻撃力
+    private int mp = 53; //魔力の量
+    //攻撃の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+
+    }
+
+    //防御の関数
+    public void Deffence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+
+        this.hp -= damage;
+
+
+    }
+
+    //魔法の関数
+    public void Magic(int cost)
+    {
+        while(this.mp>=5)                 //MPが5になるまで、繰り返しMP消費
+        {
+            this.mp -= cost;
+        
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+
+         }
+          
+           
+        Debug.Log("MPが足りないため、魔法が使えない。");
+
+     }
+}
+
 public class Test : MonoBehaviour
 {
     private void Start()
     {
-       
-        int[] array ={ 30, 60, 90, 120, 150 }; //要素が５個の配列arrayの宣言,初期化
+        Boss midboss = new Boss();
 
-        for (int i = 0; i<array.Length; i++)    //配列の各要素の値を順番に表示させる
-        {
-            Debug.Log(array[i]);
-        }
-
-        for (int j = array.Length - 1; j >= 0; j--)　　//配列の各要素の値を逆順に表示させる
-        {
-            Debug.Log(array[j]);
-        }
-
+        midboss.Attack();
+        midboss.Deffence(8);
+        midboss.Magic(5);
+    
     }
-    void Update()
-
+    private void Update()
     {
 
     }
